7ea12f5e045e731c1b958e22f56ade3012bf1366
Esra <esra.ashoor.e@gmail.com>
2024-03-12 13:03:45 +0300

    working in the form

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
-<script setup></script>
+<script>
+// import StudentForm from './components/StudentForm.vue'
+import AddCourse from './components/AddCourse.vue'
+export default {
+  name: 'App',
+  components: {
+    // StudentForm
+    AddCourse
+  }
+}
+</script>
 
 <template>
   <div>student GPA</div>
+  <AddCourse/>
+  <!-- <StudentForm /> -->
 </template>
